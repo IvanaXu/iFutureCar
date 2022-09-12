b51cc221c3f4c7b4bc9d85944a9229c092aae47b
IvanaXu <1440420407@qq.com>
2022-09-12 10:13:41 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,21 @@
-python /workspace/inference/inference_demo.py
+
+if [ "$1" = "train" ];
+then
+    echo "train"
+    python -m train.main
+elif [ "$1" = "rcmd" ]
+then
+    echo "rcmd"
+    docker run -dit --name matrix_demo registry.cn-shanghai.aliyuncs.com/tianchi/matrix:matrix_demo bash
+
+    docker cp run.sh matrix_demo:/workspace/
+    docker cp inference/ matrix_demo:/workspace/  # 场景评分代码
+    docker cp train/ matrix_demo:/workspace/ # 场景训练代码
+
+    dimage=registry.cn-shanghai.aliyuncs.com/tianchi_ivanxu/test-car:v1.0
+    docker commit matrix_demo $dimage
+    docker push $dimage
+
+else
+    python /workspace/inference/inference_demo.py
+fi
