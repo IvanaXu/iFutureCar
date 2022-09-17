8009c10c21a38efb901177de32655c6edbae97d5
IvanaXu <1440420407@qq.com>
2022-09-17 16:46:19 +0800

    Test num_episode=100&batch_size4096//2//2//2 /network_90.pth p1.6

--- train/config.py
+++ train/config.py
@@ -15,7 +15,7 @@ class PolicyParam:
 
     num_workers: int = 10
     num_episode: int = 100 # 100000
-    batch_size: int = 4096//2//2
+    batch_size: int = 4096//2//2//2
     minibatch_size: int = 256
     num_epoch: int = 10
     save_num_episode: int = 10
@@ -42,7 +42,7 @@ class PolicyParam:
 
     reload = False
 
-    model_path: str = "/myspace/results/model_10086/network_1590.pth" # None
+    model_path: str = "/myspace/results/model_10086/network_90.pth" # None
     obs_type: str = "vec"
     img_width: int = 224
     img_length: int = 224
